709e1b88e911d3348487ca6ffc72d9a15e48637a
nettlespike <46365058+nettlespike@users.noreply.github.com>
2021-09-28 22:16:52 -0400

    Update jeannifer.js

--- listeners/jeannifer.js
+++ listeners/jeannifer.js
@@ -3,8 +3,8 @@ module.exports ={
     name: "jeannifer",
     description:"ew",
     execute(message){
-        if (message.member.id === "620409343568773131") {
-            message.channel.send("ew");
-        }    
+       // if (message.member.id === "620409343568773131") {
+         //   message.channel.send("ew");
+       // }    
     }
 }
