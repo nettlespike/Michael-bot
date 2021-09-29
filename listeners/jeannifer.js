10edb1351a4efbac8b0af0ac015d64b8115a0a17
nettlespike <46365058+nettlespike@users.noreply.github.com>
2021-09-28 21:55:51 -0400

    Create jeannifer.js

---
+++ listeners/jeannifer.js
@@ -0,0 +1,10 @@
+
+module.exports ={
+    name: "jeannifer",
+    description:"ew",
+    execute(message){
+        if (message.member.id === "620409343568773131") {
+            message.channel.send("ew");
+        }    
+    }
+}
